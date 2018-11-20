46b29d0d7193607fd3d98f4d6d580c4e97003795
powderpower <ace7upp@gmail.com>
2018-11-20 21:46:51 +0300

    initial_commit

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function setUp()
+    {
+        //calls for each test
+    }
+
+    public function testValueTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
